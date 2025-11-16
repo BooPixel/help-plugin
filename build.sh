e9ffc9ada668e3d97f32f2c78c80f6b4f4f868c5
Fernando Celmer <email@fernandocelmer.com>
2025-11-16 17:33:42 -0300

    Add build script and package.json

---
+++ build.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+ZIP_NAME="help-plugin.zip"
+
+[ -f "$ZIP_NAME" ] && rm "$ZIP_NAME"
+
+zip -r "$ZIP_NAME" . \
+    -x "*.git*" \
+    -x "*.DS_Store*" \
+    -x "*__MACOSX*" \
+    -x "*.zip" \
+    -x "build.sh" \
+    -x ".gitignore" \
+    -x "README.md" \
+    -x "*.md" \
+    > /dev/null 2>&1
